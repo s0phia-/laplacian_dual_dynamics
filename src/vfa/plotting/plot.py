30cfd366fffb033a1ef775c13ff2e561572acdc6
Sophia <sophiajones@live.co.uk>
2024-05-29 15:46:46 +0100

    script to plot return vs steps

---
+++ src/vfa/plotting/plot.py
@@ -0,0 +1,39 @@
+import matplotlib as plt
+import plotnine as p9
+import pandas as pd
+import numpy as np
+
+
+class Plot:
+    def __init__(self, data_source, data_source_2=None):
+        self.data = pd.read_csv(data_source)
+        self.data_2 = pd.read_csv(data_source_2)
+
+    def plot_scatter(self):
+        p = (p9.ggplot(data=self.data,
+                       mapping=p9.aes(x='steps',
+                                      y='avg_return',
+                                      color='datapoints'))
+             + p9.geom_point()
+             + p9.theme_bw()
+             + p9.labs(color='Data points seen', x="Steps", y="Return")
+             )
+        print(p)
+
+    def plot_two(self):
+        data1 = self.data.copy()
+        data2 = self.data_2.copy()
+        data1["agent"] = "PVF"
+        data2["agent"] = "Q learning"
+        data = pd.concat([data1, data2])
+        p = (p9.ggplot(data=data,
+                       mapping=p9.aes(x='steps',
+                                      y='avg_return',
+                                      color='agent',
+                                      alpha='datapoints'))
+             + p9.geom_point()
+             + p9.scale_alpha_continuous(range=[0.1, 1])
+             + p9.theme_bw()
+             + p9.labs(color='Data points seen', x="Steps", y="Return")
+             )
+        print(p)
