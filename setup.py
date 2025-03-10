713342dfd7b8e56e8f42e654133bb9d19c01bca9
Sophia <sophiajones@live.co.uk>
2025-03-10 15:41:00 +0000

    add setup file

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name="laplacian_dual_dynamics",
     version="0.1",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    package_dir={"laplacian_dual_dynamics": "src"},
+    packages=["laplacian_dual_dynamics"] + ["laplacian_dual_dynamics." + pkg for pkg in find_packages(where="src")],
 )
